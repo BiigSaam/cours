196c860b26362539b49a23d36ad6a3d44d515171
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-22 13:22:48 +0200

    creation-et-design-interactif-s4 - wip add shooting rate for plant enemy

--- creation-et-design-interactif-s4/samples/advanced-base-godot/Scripts/PlantEnemy.gd
+++ creation-et-design-interactif-s4/samples/advanced-base-godot/Scripts/PlantEnemy.gd
@@ -8,6 +8,7 @@ signal on_detect_area_enter(body)
 func _on_area_2d_body_entered(body):
 #	$Animator.play("shoot")
 	print(body)
+	shoot_rate.start()
 #	var bullet = bullet_prefab.instantiate() as Area2D
 #
 #	add_child(bullet)
@@ -15,9 +16,12 @@ func _on_area_2d_body_entered(body):
 
 
 
-func _on_animator_animation_finished():
-	print("Heeedd")
-#	$Animator.play("idle")
+func _on_animator_animation_finished(anim_name):
+	print("Heeedd " + anim_name)
+	if anim_name == "shoot":
+		$Animator.play("idle")
 
 func shoot():
+	var bullet = bullet_prefab.instantiate() as Area2D
+	add_child(bullet)
 	pass
