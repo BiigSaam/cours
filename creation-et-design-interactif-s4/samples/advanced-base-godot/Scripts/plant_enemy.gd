ef066808303df4c56cff63711fe7c1c849e509df
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-23 00:24:29 +0200

    creation-et-design-interactif-s4 - Add hit animation for enemy plant

--- creation-et-design-interactif-s4/samples/advanced-base-godot/Scripts/plant_enemy.gd
+++ creation-et-design-interactif-s4/samples/advanced-base-godot/Scripts/plant_enemy.gd
@@ -6,9 +6,11 @@ extends RigidBody2D
 @export var data:EnemyData
 
 var health = 0
+var animator = null
 
 func _ready():
 	health = data.max_health
+	animator = $Animator
 	shoot_rate = get_node("ShootRate")
 
 func _on_shoot_area_entered(body):
@@ -19,8 +21,8 @@ func _on_shoot_area_exited(body):
 	shoot_rate.stop()
 
 func _on_animator_animation_finished(anim_name):
-	if anim_name == "shoot":
-		$Animator.play("idle")
+	if anim_name != "idle":
+		animator.play("idle")
 
 func shoot():
 	var bullet = bullet_prefab.instantiate() as RigidBody2D
@@ -34,19 +36,27 @@ func shoot():
 #	self.add_child(bullet)
 
 func _on_shoot_rate_timeout():
-	$Animator.play("shoot")
-
-func _on_body_entered(body):
-	print(body)
-	print(body.position.y)
+	animator.play("shoot")
 
 func _on_hit_area_entered(body):
 	if "velocity" in body:
 		body.velocity = Vector2(body.velocity.x, data.bounce_force)
+		animator.play("hit")
 		health -= 1
 		if health <= 0:
 			die()
 
 func die():
-	queue_free()
+	set_collision_mask_value(2, false)
+	set_collision_mask_value(1, false)
+	$EnemyCollision.set_deferred("disabled", true)
+	$HitArea.set_deferred("disabled", true)
+	$ShootArea.set_deferred("disabled", true) 
 	
+	apply_force(Vector2.UP * 12000)
+	rotation_degrees = 45
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	if health <= 0:
+		queue_free()
